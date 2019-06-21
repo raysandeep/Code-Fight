003e070a3159fdcd168ac58756a2b879a3cbd3ee
Sai Sandeep Rayanuthala <43823311+unknown-guy-1610@users.noreply.github.com>
2019-06-22 00:36:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -17,7 +17,8 @@ def index():
 
 @app.route('/api/say_name', methods=['POST'])
 def say_name():
-    token = "Your Token"
+
+    token = "TOKEN"
     json = request.get_json()
     org = json['first_name']
     org1 = json['last_name']
@@ -38,7 +39,7 @@ def say_name():
 
 @app.route('/api/contribution', methods=['POST'])
 def contribution():
-    token = "Your Token"
+    token = "TOKEN"
     json = request.get_json()
     name = json['first_name']
     org = json['last_name']
@@ -59,6 +60,46 @@ def contribution():
             score = "check name properly" 
 
     return jsonify(first_name=name, last_name=score)
+
+@app.route('/api/compare', methods=['POST'])
+def compare():
+    token = "TOKEN"
+    json = request.get_json()
+    name = json['first_name']
+    org = json['org1']
+    org1 = "GDGVIT"
+    redis = None
+    res = utility.leaderboard(token, org,redis)
+    scr = 0
+    scr1 =0
+    print(res)
+    for key in res:
+        scr +=  res[key]
+    
+    for i in res:
+        if name == i:
+            scr1 = res[name]
+            score = 0
+            score = round((scr1/scr)*100,2)
+        else :
+            score = "check name properly" 
+    res1 = utility.leaderboard(token, org1,redis)
+    scr2 = 0
+    scr3 =0
+    print(res1)
+    for key in res1:
+        scr3 +=  res1[key]
+    
+    for i in res:
+        if name == i:
+            scr2 = res[name]
+            score = 0
+            score1 = round((scr2/scr3)*100,2)
+        else :
+            score1 = "check name properly" 
+    word = name + " Contribution towards " + org + " is "+ score
+    word1 ="and Contribution towards " + org1 + " is "+ score1
+    return jsonify(first_name=word, last_name=word1)
 
 if __name__ == '__main__':
     app.run(debug=True)
